ad88d927c2d284488cbb72d1d5e498a1e88f333b
giamanpanos <90336670+giamanpanos@users.noreply.github.com>
2025-04-05 15:29:36 +0300

    Update UpdateProfileForm with the country flag

--- app/_components/UpdateProfileForm.js
+++ app/_components/UpdateProfileForm.js
@@ -37,11 +37,11 @@ function UpdateProfileForm({ guest, children }) {
       <div className="space-y-2">
         <div className="flex items-center justify-between">
           <label htmlFor="nationality">Where are you from?</label>
-          <img
+          {nationality && <img
             src={countryFlag}
             alt="Country flag"
             className="h-5 rounded-sm"
-          />
+          />}
         </div>
 
         {children}
